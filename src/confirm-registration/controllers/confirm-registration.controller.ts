ffbbed77947b9f3c6104ae09061448ac45ef7d8c
Danila Litvishka <litvishko30@mail.ru>
2021-07-07 13:38:08 +0300

    added return type

--- src/confirm-registration/controllers/confirm-registration.controller.ts
+++ src/confirm-registration/controllers/confirm-registration.controller.ts
@@ -14,7 +14,7 @@ export class ConfirmRegistrationController {
     }
 
     @Get('/resend-email/:email')
-    resendEmail(@Param('email') email:string):any{
+    resendEmail(@Param('email') email:string):Promise<void>{
         return this.confirmRegistrationService.resendEmail(email);
     }
 }
